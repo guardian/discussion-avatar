2a2b949a740f3cae4adb8e9964cca2c824aa133a
Nicolas Long <nicolaslong@gmail.com>
2015-03-20 11:06:25 +0000

    Re-add with filename change

---
+++ src/main/scala/com/gu/entities/errors.scala
@@ -0,0 +1,18 @@
+package com.gu.entities
+
+import scalaz.NonEmptyList
+
+sealed trait Error {
+  val message: String
+  val errors: NonEmptyList[String]
+}
+case class InvalidFilters(message: String, errors: NonEmptyList[String]) extends Error
+case class AvatarNotFound(message: String, errors: NonEmptyList[String]) extends Error
+
+object Errors {
+  def invalidFilters(errors: NonEmptyList[String]): InvalidFilters =
+    InvalidFilters("Invalid filter parameters", errors)
+
+  def avatarNotFound(errors: NonEmptyList[String]): AvatarNotFound =
+    AvatarNotFound("Avatar not found", errors)
+}
