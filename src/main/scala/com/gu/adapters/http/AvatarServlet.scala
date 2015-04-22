950f86c0f5c170dd7808862a711467fd772cef6c
Nicolas Long <nicolaslong@gmail.com>
2015-04-22 16:54:32 +0100

    Inline handling

--- src/main/scala/com/gu/adapters/http/AvatarServlet.scala
+++ src/main/scala/com/gu/adapters/http/AvatarServlet.scala
@@ -1,6 +1,7 @@
 package com.gu.adapters.http
 
 import com.gu.adapters.store.Store
+import com.gu.core.Errors._
 import com.gu.core._
 import org.json4s.ext.JodaTimeSerializers
 import org.json4s.{DefaultFormats, Formats}
@@ -8,6 +9,8 @@ import org.scalatra._
 import org.scalatra.json.JacksonJsonSupport
 import org.scalatra.servlet.{FileUploadSupport, MultipartConfig, SizeConstraintExceededException}
 import org.scalatra.swagger.{Swagger, SwaggerSupport}
+
+import scalaz.{-\/, NonEmptyList, \/, \/-}
 
 class AvatarServlet(store: Store)(implicit val swagger: Swagger)
   extends ScalatraServlet
@@ -31,25 +34,70 @@ class AvatarServlet(store: Store)(implicit val swagger: Swagger)
     case e: SizeConstraintExceededException => RequestEntityTooLarge(ErrorResponse("File exceeds size limit: images must be no more than 1mb in size", Nil))
   }
 
-  get("/service/healthcheck")(Handler.healthcheck())
-  get("/service/gtg")(Handler.gtg())
-  get("/service/dependencies")(Handler.dependencies())
+  get("/service/healthcheck") {
+    Ok(Message("OK"))
+  }
+  get("/service/gtg") {
+    NotImplemented(Message("Endpoint needs to be specified"))
+  }
+  get("/service/dependencies") {
+    NotImplemented(Message("Endpoint needs to be specified"))
+  }
 
-  get("/avatars", operation(getAvatars))(Handler.getAvatars(store, params))
-  get("/avatars/:id", operation(getAvatar))(Handler.getAvatar(store, params("id")))
+  get("/avatars", operation(getAvatars)) {
+    withErrorHandling {
+      Filters.fromParams(params) flatMap store.get
+    }
+  }
 
-  get("/avatars/user/:userId", operation(getAvatarsForUser))(
-    Handler.getAvatarsForUser(store, params("userId")))
+  get("/avatars/:id", operation(getAvatar)) {
+    withErrorHandling {
+      store.get(params("id"))
+    }
+  }
 
-  get("/avatars/user/:userId/active", operation(getActiveAvatarForUser))(
-    Handler.getActiveAvatarForUser(store, User(params("userId").toInt)))
+  get("/avatars/user/:userId", operation(getAvatarsForUser)) {
+    withErrorHandling {
+      val user = User(params("userId").toInt)
+      store.get(user)
+    }
+  }
 
-  post("/avatars", operation(postAvatar))(Handler.postAvatar(
-    store, request.contentType,
-    request.body,
-    fileParams("image")))
+  get("/avatars/user/:userId/active", operation(getActiveAvatarForUser)) {
+    withErrorHandling {
+      val user = User(params("userId").toInt)
+      store.getActive(user)
+    }
+  }
 
-  put("/avatars/:id/status", operation(putAvatarStatus))(Handler.putAvatarStatus(store, request.body, params("id")))
+  post("/avatars", operation(postAvatar)) {
+    withErrorHandling {
+      //    val cd = new IdentityCookieDecoder(new ProductionKeys)
+
+      //    for {
+      //      cookie <- request.cookies.get("GU_U")
+      //      user <- cd.getUserDataForGuU(cookie).map(_.user)
+      //      username <- user.publicFields.displayName
+      //    }
+
+      val user = User("123456".toInt)
+      val url = (parse(request.body) \ "url").values.toString
+      val image = fileParams("image")
+
+      request.contentType match {
+        case Some("application/json") | Some("text/json") => store.fetchImage(user, url)
+        case Some(s) if s startsWith "multipart/form-data" => store.userUpload(user, image)
+        case Some(invalid) => -\/(invalidContentType(NonEmptyList(s"'$invalid' is not a valid content type. Must be 'multipart/form-data' or 'application/json'.")))
+      }
+    }
+  }
+
+  put("/avatars/:id/status", operation(putAvatarStatus)) {
+    withErrorHandling {
+      val status = Status((parse(request.body) \ "status").values.toString) // TODO handle errors gracefully here
+      store.updateStatus(params("id"), status)
+    }
+  }
 
   //get("/avatars/user/me/active", operation(getActiveAvatarForUserInfo))(getActiveAvatarForUser())
   //get("/avatars/stats", operation(getAvatarStatsInfo))(getAvatarStats)
@@ -57,4 +105,30 @@ class AvatarServlet(store: Store)(implicit val swagger: Swagger)
   // for cdn endpoint (avatars.theguardian.com)
   //   /user/:id -> retrieve active avatar for a user
   //   /user/me  -> retrieve active avatar for me (via included cookie)
+
+  def withErrorHandling(response: => \/[Error, Any]): ActionResult = {
+    (handleSuccess orElse handleError)(response)
+  }
+
+  def redirectOrError(response: \/[Error, Any]): ActionResult = {
+    (handleRedirect orElse handleError)(response)
+  }
+
+  def handleSuccess: PartialFunction[\/[Error, Any], ActionResult] = {
+    case \/-(success) => Ok(success)
+  }
+
+  def handleRedirect: PartialFunction[\/[Error, Any], ActionResult] = {
+    case \/-(success) => TemporaryRedirect(success.toString)
+  }
+
+  def handleError[A]: PartialFunction[\/[Error, A], ActionResult] = {
+    case -\/(error) => error match {
+      case InvalidContentType(msg, errors) => UnsupportedMediaType(ErrorResponse(msg, errors.list))
+      case InvalidFilters(msg, errors) => BadRequest(ErrorResponse(msg, errors.list))
+      case AvatarNotFound(msg, errors) => NotFound(ErrorResponse(msg, errors.list))
+      case AvatarRetrievalFailed(msg, errors) => ServiceUnavailable(ErrorResponse(msg, errors.list))
+      case DynamoRequestFailed(msg, errors) => ServiceUnavailable(ErrorResponse(msg, errors.list))
+    }
+  }
 }
