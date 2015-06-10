89883081c725515248f88346ac3b4932fb3cb0e4
Nick Satterly <nick.satterly@guardian.co.uk>
2015-06-10 16:59:22 +0100

    Remove unused import and put case class on one line

--- src/main/scala/com/gu/adapters/http/params.scala
+++ src/main/scala/com/gu/adapters/http/params.scala
@@ -1,16 +1,11 @@
 package com.gu.adapters.http
 
-import java.util.UUID
-
 import com.gu.core.Status
 import org.joda.time.DateTime
 
 sealed trait RequestParam
 case class StatusRequest(status: Status)
-case class AvatarRequest(
-  url: String,
-  isSocial: Boolean
-)
+case class AvatarRequest(url: String, isSocial: Boolean)
 
 case class MigratedAvatarRequest(
   userId: Int,
