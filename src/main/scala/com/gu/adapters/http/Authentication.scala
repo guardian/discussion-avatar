5d18e475bc0d7b77c206f4bc29ef98070a274724
Nick Satterly <nick.satterly@guardian.co.uk>
2015-06-10 19:34:13 +0100

    Simplify isValidKey method by using case class instead of for/yield

--- src/main/scala/com/gu/adapters/http/Authentication.scala
+++ src/main/scala/com/gu/adapters/http/Authentication.scala
@@ -5,7 +5,7 @@ import com.gu.core.Errors._
 import com.gu.core.{ Config, Error, User }
 import com.gu.identity.cookie.IdentityCookieDecoder
 
-import scalaz.std.option.optionSyntax._
+import scalaz.Scalaz._
 import scalaz.{ NonEmptyList, \/ }
 
 object TokenAuth {
@@ -15,15 +15,14 @@ object TokenAuth {
   def isValidKey(authHeader: Option[String]): Error \/ String = {
 
     val tokenHeader = "Bearer token="
-    val tokenOption = authHeader.withFilter(_.startsWith(tokenHeader)).map(_.stripPrefix(tokenHeader))
+    val token = authHeader.withFilter(_.startsWith(tokenHeader)).map(_.stripPrefix(tokenHeader))
 
-    val found = for {
-      token <- tokenOption.toRightDisjunction("No access token in request")
-    } yield token
-
-    found
-      .ensure("Invalid access token provided")(apiKeys.contains)
-      .leftMap(error => tokenAuthorizationFailed(NonEmptyList(error)))
+    val tokenOrError = token match {
+      case Some(valid) if apiKeys.contains(valid) => valid.right
+      case Some(invalid) => "Invalid access token provided".left
+      case None => "No access token in request".left
+    }
+    tokenOrError.leftMap(error => tokenAuthorizationFailed(NonEmptyList(error)))
   }
 }
 
