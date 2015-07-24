f0db3cd68d1bd6aae3264ce71756f5e9d4018dc6
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-07-24 15:13:47 +0100

    include status code when logging error

--- api/src/main/scala/com/gu/adapters/utils/ErrorHandling.scala
+++ api/src/main/scala/com/gu/adapters/utils/ErrorHandling.scala
@@ -36,14 +36,14 @@ object ErrorHandling extends LazyLogging {
 
   def ioError(e: Throwable): Error = ioFailed(NonEmptyList(e.getMessage))
 
-  def logError(msg: String, e: Error): Error = {
+  def logError(msg: String, statusCode: Option[Int] = None, e: Error): Error = {
     val errors = e.message + " " + e.errors.toList.mkString("(", ", ", ")")
-    logger.error(msg + " - cause is: " + errors)
+    logger.error(s"$msg - $statusCode  - cause is: $errors")
     e
   }
 
   def logIfError[A](msg: String, result: Error \/ A): Error \/ A = {
-    result.bimap(e => logError(msg, e), identity)
+    result.bimap(e => logError(msg = msg, e = e), identity)
   }
 
 }
