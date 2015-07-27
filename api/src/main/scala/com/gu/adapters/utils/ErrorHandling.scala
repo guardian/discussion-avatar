765fd77250f3664c21d9807cd9cc59fda796dcf1
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-07-27 18:05:01 +0100

    only show status code in log line if it exists

--- api/src/main/scala/com/gu/adapters/utils/ErrorHandling.scala
+++ api/src/main/scala/com/gu/adapters/utils/ErrorHandling.scala
@@ -38,7 +38,10 @@ object ErrorHandling extends LazyLogging {
 
   def logError(msg: String, statusCode: Option[Int] = None, e: Error): Error = {
     val errors = e.message + " " + e.errors.toList.mkString("(", ", ", ")")
-    logger.error(s"$msg - Status code=${statusCode.getOrElse("None")} - cause is: $errors")
+    statusCode match {
+      case Some(statusCode) => logger.error(s"$msg - status code=${statusCode} - cause is: $errors")
+      case _ => logger.error(s"$msg - cause is: $errors")
+    }
     e
   }
 
